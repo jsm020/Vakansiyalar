351bee5ee281736ce607a9965eae0c06dc8d8819
Javoxir Ixmatullayev <78523543+jsm020@users.noreply.github.com>
2025-06-30 11:53:03 +0000

    Register User model in Django admin with custom display, search, and filter options

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import User
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("id", "username", "first_name", "last_name", "middle_name", "phone", "is_active", "is_staff", "date_joined")
+    search_fields = ("username", "first_name", "last_name", "phone")
+    list_filter = ("is_active", "is_staff", "date_joined")
