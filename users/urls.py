6feef46457d3fbdd3f61a6f832119a8a680922e5
Javoxir Ixmatullayev <78523543+jsm020@users.noreply.github.com>
2025-06-30 11:57:45 +0000

    Add DRF-YASG for API documentation and update URL configurations

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,13 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from . import views
+
+router = DefaultRouter()
+# Router faqat ViewSet lar uchun ishlaydi, APIView lar uchun path ishlatiladi
 
 urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='register'),
     path('login/', views.LoginView.as_view(), name='login'),
     path('recovery/', views.PasswordRecoveryView.as_view(), name='password_recovery'),
+    path('', include(router.urls)),
 ]
