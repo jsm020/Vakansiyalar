817ead0d47296f54d2c8f31366ad1c4d597b0167
Javoxir Ixmatullayev <78523543+jsm020@users.noreply.github.com>
2025-07-03 06:02:16 +0000

    Add DiplomaAdmin to admin panel for diploma management

--- users/admin.py
+++ users/admin.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
 
+
 from django.contrib import admin
-from .models import User
+from .models import User, Diploma
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ("id", "username", "first_name", "last_name", "middle_name", "phone", "is_active", "is_staff", "date_joined")
     search_fields = ("username", "first_name", "last_name", "phone")
     list_filter = ("is_active", "is_staff", "date_joined")
+
+
+@admin.register(Diploma)
+class DiplomaAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "specialization", "graduation_year", "diploma_number", "created_at")
+    search_fields = ("user__username", "specialization", "diploma_number")
+    list_filter = ("graduation_year", "specialization")
